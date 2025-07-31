db84eb4af896f104149eee969e8ea1b3f98dfb68
Veer Pratap Singh <xyresiiic@gmail.com>
2025-07-31 21:30:23 +0530

    Update login and sign up.cpp

--- login and sign up.cpp
+++ login and sign up.cpp
@@ -5,23 +5,23 @@ class signup {
 public:
     string Username;
     string Password;
-    string user; 
-    string pass; 
-    string suser;
-    string spass;
+    string suser; 
+    string spass; 
+    string luser;
+    string lpass;
     int option;
 };
 
+signup s1; 
 
-signup s1;
-
-void signup() {
+void doSignup() {
     cout << "Enter username: ";
-    cin >> s1.user;
-    cout << "Enter password: ";
-    cin >> s1.pass;
+    cin >> s1.suser;
 
-    if (s1.user == s1.Username && s1.pass == s1.Password) {
+    cout << "Enter password: ";
+    cin >> s1.spass;
+
+    if (s1.suser == s1.Username && s1.spass == s1.Password) {
         cout << "Welcome, " << s1.Username << endl;
     } else {
         cout << "Access denied. Invalid Inputs." << endl;
@@ -29,18 +29,21 @@ void signup() {
 }
 
 void login() {
-
     cout << "Enter username: ";
-    cin >> s1.suser;
+    cin >> s1.luser;
 
     cout << "Enter password: ";
-    cin >> s1.spass;
-       
+    cin >> s1.lpass;
+
+    if (s1.luser == s1.Username && s1.lpass == s1.Password) {
+        cout << "Login successful! Welcome back, " << s1.Username << "!" << endl;
+    } else {
+        cout << "Login failed. Invalid username or password." << endl;
+    }
 }
 
 int main() {
-    
-
+	cout << "Welcome! to signUp page"<<endl;
     cout << "Enter username: ";
     cin >> s1.Username;
 
@@ -49,19 +52,17 @@ int main() {
 
     cout << "Signup successful! " << s1.Username << endl;
 
-    cout << "1.Press 1 for login"<<endl;
-    cout << "2.Press 2 for signup"<<endl;
+    cout << "1. Press 1 for login" << endl;
+    cout << "2. Press 2 for signup" << endl;
     cin >> s1.option;
 
     if (s1.option == 1) {
-        signup();
-        
-    } else if (s1.option == 2){
-       login();
+        login();
+    } else if (s1.option == 2) {
+        doSignup();
+    } else {
+        cout << "Invalid input" << endl;
     }
-    else{
-    	cout << "Invaid input";
-    	
-	}  
-}
 
+    return 0;
+}
